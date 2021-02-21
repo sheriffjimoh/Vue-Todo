befd9537cfe76abc344c8f16421102b54b64686e
Sherifdeen <jimohsherifdeen6@gmail.com>
2021-02-21 08:29:30 -0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div id="app">
+   <Header />
+   <AddTodo  v-on:add-todo="addTodo" />
+  <Todos  v-bind:todos="todos"   v-on:del-todo="deleteTodo" />
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/layout/header';
+import AddTodo from './components/AddTodo';
+import Todos from './components/Todos';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    AddTodo,
+    Todos
+  },
+ 
+  data() {
+    return {
+      todos:[
+        {
+          id:1,
+          title:"Todo One",
+          completed:false
+        },
+         {
+          id:2,
+          title:"Todo Two",
+          completed:true
+        },
+         {
+          id:3,
+          title:"Todo Three",
+          completed:false
+        }
+      ]
+    }
+  },
+
+  methods:{
+    deleteTodo(id){
+     this.todos = this.todos.filter(todo => todo.id !==id)
+    }
+  },
+  addTodo(newTodo){
+    this.todos = [...this.todos, newTodo];
   }
 }
 </script>
@@ -22,5 +60,19 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  padding: 3px;
+}
+
+.btn{
+  display: inline-block;
+  border: none;
+  background: #5555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover{
+  background:#666;
 }
 </style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,20 @@
+<template>
+    <div v-for="todo in todos"  :key="todo.id" >
+      <TodoItem v-bind:todo="todo"  v-on:del-todo="$emit('del-todo', todo.id)"/>
+    </div>
+</template>
+
+<script>
+import TodoItem from './Todoitem.vue';
+export default {
+    name:"Todos",
+    props:["todos"],
+    components:{
+        TodoItem
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
